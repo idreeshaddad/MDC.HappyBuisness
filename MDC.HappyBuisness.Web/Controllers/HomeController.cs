a3819e7404edddd47e957dab0d9a01e8df8e4b26
Idrees Haddad <gelnox@live.com>
2022-07-31 19:29:45 +0300

    Added HomePageViewModel

--- MDC.HappyBuisness.Web/Controllers/HomeController.cs
+++ MDC.HappyBuisness.Web/Controllers/HomeController.cs
@@ -6,11 +6,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using MDC.HappyBuisness.Web.Models.Drugs;
+using MDC.HappyBuisness.Web.Models.Home;
 
 namespace MDC.HappyBuisness.Web.Controllers
 {
     public class HomeController : Controller
     {
+        #region Data and Constructor
+
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _context;
         private readonly IMapper _mapper;
@@ -22,17 +25,19 @@ namespace MDC.HappyBuisness.Web.Controllers
             _mapper = mapper;
         }
 
+        #endregion
+
+        #region Actions
+
         public async Task<IActionResult> Index()
         {
-            var drugs = await _context
-                            .Drugs
-                            .OrderByDescending(d => d.Rating)
-                            .Take(3)
-                            .ToListAsync();
+            var homePageVM = new HomePageViewModel();
 
-            var drugVMS = _mapper.Map<List<Drug>, List<DrugListViewModel>>(drugs);
+            homePageVM.TopRatedDrugs = await GetTopRated3Drugs();
+            homePageVM.NumberOfBuyers = await GetNumberOfBuyers();
 
-            return View(drugVMS);
+            
+            return View(homePageVM);
         }
 
         public IActionResult Privacy()
@@ -44,6 +49,29 @@ namespace MDC.HappyBuisness.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        } 
+
+        #endregion
+
+        #region Private Methods
+
+        private async Task<List<DrugListViewModel>> GetTopRated3Drugs()
+        {
+            var top3ratedDrugs = await _context
+                                        .Drugs
+                                        .OrderByDescending(d => d.Rating)
+                                        .Take(3)
+                                        .ToListAsync();
+
+            var top3ratedDrugsVms = _mapper.Map<List<Drug>, List<DrugListViewModel>>(top3ratedDrugs);
+            return top3ratedDrugsVms;
         }
+
+        private async Task<int> GetNumberOfBuyers()
+        {
+            return await _context.Buyers.CountAsync();                  
+        }
+
+        #endregion
     }
 }
---
+++ MDC.HappyBuisness.Web/Models/Home/HomePageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MDC.HappyBuisness.Web.Models.Drugs;
+
+namespace MDC.HappyBuisness.Web.Models.Home
+{
+    public class HomePageViewModel
+    {
+        public HomePageViewModel()
+        {
+            TopRatedDrugs = new List<DrugListViewModel>();
+        }
+
+        public List<DrugListViewModel> TopRatedDrugs { get; set; }
+
+        public int NumberOfBuyers { get; set; }
+    }
+}
